a5f35661b06b26f5d73d0e8f60680a336154de7a
Sawyer06 <sawblade0@outlook.com>
2021-05-30 16:40:03 -0400

    gauge to show battery level finished

--- Facility/Assets/Scripts/BatteryGauge.cs
+++ Facility/Assets/Scripts/BatteryGauge.cs
@@ -4,74 +4,54 @@ using UnityEngine;
 
 public class BatteryGauge : MonoBehaviour
 {
-    float t;
+    //float t;
     public GameObject myBattery;
     public GameObject orgin;
     public float batteryLife;
-    public float turnDeg;
+    //public float turnDeg;
     public float startPos;
     public Quaternion endPos;
     private float goal;
-    private float needleRot;
+    //private float needleRot;
     private bool startTimer;
     private Vector3 eulers;
     public float degreesPerSecond;
     public float totalRotationTime;
     private bool canRotate;
+    
 
     void Start()
     {
-        //eulers = transform.rotation.eulerAngles;
-        //orgin.transform.rotation = startPos;
         canRotate = true;
         goal = endPos.eulerAngles.z;
+        
     }
 
 
     void Update()
-    {
+    {       
+        //Debug.Log(canRotate);
         batteryLife = myBattery.GetComponent<Battery>().batteryLife;
         startTimer = myBattery.GetComponent<Battery>().startTimer;
         //totalRotationTime = batteryLife;
-        degreesPerSecond = goal / totalRotationTime;
-        if (orgin.transform.eulerAngles.z >= 80 && startTimer == true)
+        
+        if (startTimer == true && canRotate == true)
         {
+            degreesPerSecond = goal / totalRotationTime - 0.65f;
+            
             orgin.transform.Rotate(0, 0, degreesPerSecond * Time.deltaTime);
+            
         }
         if (batteryLife == 0)
         {
+            canRotate = false;
             transform.rotation = Quaternion.Euler(0, 0, 80);
         }
-        /*
-        if (orgin.transform.eulerAngles.z == 180)
+        if (transform.eulerAngles.z == 80)
         {
             canRotate = false;
+            degreesPerSecond = 0;
         }
-        */
-        /*if (startTimer == true)
-        {
-            StartCoroutine(NeedleRotate(eulers));
-        }
-        if (startTimer == false)
-        {
-            StopCoroutine(NeedleRotate(eulers));
-        }
-        orgin.transform.rotation = Quaternion.Euler(eulers.x, eulers.y, needleRot);
-        */
     }
-
-    /*IEnumerator NeedleRotate(Vector3 eulers)
-    {
-        while (startTimer == true)
-        {
-            
-            if (startTimer == false)
-            {
-                break;
-            }
-            yield return null;
-        }
-        
-    }
-    */
+    
 }
