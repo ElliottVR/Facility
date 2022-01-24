28a2974ffb25d8464e8d618157787e111cd1c1ad
R.Elliott <59777872+ElliottVR@users.noreply.github.com>
2022-01-24 08:13:47 -0500

    test

---
+++ Facility/Assets/Scripts/Debugger.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DebugHelper
+{
+
+    public static void HandleEditorInput(bool lockCursor)
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        if (Input.GetMouseButtonDown(0) && lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Break();
+        }
+    }
+}
